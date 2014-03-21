fd5a559e733604a15ed9ee7d57312cf48baa81f0
Tobias Bertelsen <Tobias Bertelsen@ubuntu-VirtualBox>
2014-03-21 17:33:35 +0800

    Added Eclipse project: 'TwitterData'
    Added Eclipse project which is set up for MapReduce development in the
    Linux virtual machinge given by prof. It includes:
    
     - A dummy main file
     - References to the Hadoop libaries. 
       This enables instant compilation of source code
     - Automatic builds classes.jar to deploy in hadoop

---
+++ JavaProjects/TwitterData/src/sg/edu/nus/cs5344/spring14/twitter/Main.java
@@ -0,0 +1,10 @@
+package sg.edu.nus.cs5344.spring14.twitter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO (tbertelsen) Do everything :)
+		System.out.println("Hello World");
+	}
+	
+}
