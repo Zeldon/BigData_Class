b5d17e57c3a1313b340dd6d25a104387e80c0187
Tobias Bertelsen <tobias.bertelsen@gmail.com>
2014-04-20 22:51:51 +0800

    Updating shell script to only execute trend detection

--- JavaProjects/TwitterData/run.sh
+++ JavaProjects/TwitterData/run.sh
@@ -1,6 +1,5 @@
 # The working directory, Input data must be in DATA_DIR/input
 DATA_DIR=/data/100m
 
-hadoop jar classes.jar sg.edu.nus.cs5344.spring14.twitter.TwMain -d $DATA_DIR
+hadoop jar classes.jar sg.edu.nus.cs5344.spring14.twitter.TwMain -d $DATA_DIR --skip A D E B
 hadoop fs -ls $DATA_DIR/output/
-hadoop fs -cat $DATA_DIR/output/DayStats/part-r-00000
