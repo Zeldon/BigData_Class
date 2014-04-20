8c27e9b13369928ad619e122f3f5e0d2c1993610
Tobias Bertelsen <tobias.bertelsen@gmail.com>
2014-04-21 04:35:59 +0800

    Setting to only run C and F

--- JavaProjects/TwitterData/run.sh
+++ JavaProjects/TwitterData/run.sh
@@ -1,5 +1,5 @@
 # The working directory, Input data must be in DATA_DIR/input
 DATA_DIR=/data/100m
 
-hadoop jar classes.jar sg.edu.nus.cs5344.spring14.twitter.TwMain -d $DATA_DIR --skip A D E B
+hadoop jar classes.jar sg.edu.nus.cs5344.spring14.twitter.TwMain -d $DATA_DIR --only C F
 hadoop fs -ls $DATA_DIR/output/
